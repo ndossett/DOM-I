4989ca704d7836a04010d32935e987697381c1a6
ndossett <niki.m.dossett@gmail.com>
2020-10-27 11:41:40 -0400

    finished mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,46 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+//header section
+const nav = document.querySelectorAll('nav a');
+nav[0].textContent = siteContent.nav['nav-item-1']
+nav[1].textContent = siteContent.nav['nav-item-2']
+nav[2].textContent = siteContent.nav['nav-item-3']
+nav[3].textContent = siteContent.nav['nav-item-4']
+nav[4].textContent = siteContent.nav['nav-item-5']
+nav[5].textContent = siteContent.nav['nav-item-6']
+//cta section
+const h1 = document.querySelector('h1')
+const button = document.querySelector('button')
+const src = document.querySelector('#cta-img')
+h1.textContent = siteContent.cta.h1
+button.textContent = siteContent.cta.button
+src.setAttribute('src', siteContent['cta']['img-src'])
+//top content section
+const topH4 = document.querySelectorAll('.top-content h4')
+const topP = document.querySelectorAll('.top-content p')
+topH4[0].textContent = siteContent['main-content']['features-h4']
+topP[0].textContent = siteContent['main-content']['features-content']
+topH4[1].textContent = siteContent['main-content']['about-h4']
+topP[1].textContent = siteContent['main-content']['about-content']
+
+const middleSrc = document.querySelector('#middle-img')
+middleSrc.setAttribute('src', siteContent['main-content']['middle-img-src'])
+//bottom content section
+const bottomH4 = document.querySelectorAll('.bottom-content h4')
+const bottomP = document.querySelectorAll('.bottom-content p')
+bottomH4[0].textContent = siteContent['main-content']['services-h4']
+bottomP[0].textContent = siteContent['main-content']['services-content']
+bottomH4[1].textContent = siteContent['main-content']['product-h4']
+bottomP[1].textContent = siteContent['main-content']['product-content']
+bottomH4[2].textContent = siteContent['main-content']['vision-h4']
+bottomP[2].textContent = siteContent['main-content']['vision-content']
+//contact section
+const contactH4 = document.querySelector('.contact h4')
+const contactP = document.querySelectorAll('.contact p')
+const footerP = document.querySelector('footer p')
+contactH4.textContent = siteContent['contact']['contact-h4']
+contactP[0].textContent = siteContent['contact']['address']
+contactP[1].textContent = siteContent['contact']['phone']
+contactP[2].textContent = siteContent['contact']['email']
+footerP.textContent = siteContent['footer']['copyright']
